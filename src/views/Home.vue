a5ae92a6b5ca183e447dbdd4bf9235ad08e11e4a
kczapko <kczapko@gmail.com>
2021-11-14 22:04:56 +0100

    css fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
   .page__content
     header.header-home
       h1
-        .heading-5 So, you want to travel to 
-        .heading-1 SPACE
+        span.heading-5 So, you want to travel to 
+        strong.heading-1 SPACE
       p Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
     router-link(:to="{ name: 'destination' }" class="header-home__link")
       span.heading-4 Explore
